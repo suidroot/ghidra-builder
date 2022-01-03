060e23239741dec2498f07dc6a1e248c27946080
Ben Mason <locutus@the-collective.net>
2022-01-03 15:52:17 -0500

    Moved to Gradle 7.3 added branch options and clean script

--- workdir/build_ghidra.sh
+++ workdir/build_ghidra.sh
@@ -3,12 +3,14 @@ set -ex
 
 START_DIR=$(pwd)
 OUTPUT_DIR=$START_DIR/out
+BRANCH='stable'    # master, stable, patch
+
 
 rm -rf $OUTPUT_DIR || true
 mkdir -p $OUTPUT_DIR
 
 if [ ! -d "ghidra" ]; then
-    git clone https://github.com/NationalSecurityAgency/ghidra
+    git clone -b $BRANCH https://github.com/NationalSecurityAgency/ghidra
 fi
 
 ln -s $HOME/ghidra.bin ghidra.bin # hack
@@ -17,6 +19,8 @@ cd $START_DIR/ghidra
 gradle --init-script gradle/support/fetchDependencies.gradle init
 gradle yajswDevUnpack
 gradle buildGhidra
+gradle prepDev
+gradle eclipse -PeclipsePDE
 
 # Tests
 # Xvfb :99 -nolisten tcp & export DISPLAY=:99
---
+++ workdir/clean.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm -rf ghidra
+rm -rf ghidra.bin  
+rm -rf out
